5d62a6ad31b936ca43ccee0b0befaf43f8a7e680
codefly13 <243864952@qq.com>
2016-09-01 19:34:03 +0800

    修改了原函数若干bug。将函数抽取成小函数模块。

--- LSFS/LSFS.py
+++ LSFS/LSFS.py
@@ -4,139 +4,12 @@ import scipy as sp
 import os
 import random
 import time
+import sys
 
-def norm_2_1(a):
-    """
-    对每行的向量求第二范数，然后对所有范数求和
-    """
-    return np.sum(np.linalg.norm(a,axis=1))
-
-
-def fun22_value(W, X, H, Q, Y):
-    """
-    ||H*X.T*W - H*W||F范数 ^ 2 + gama * (W.T*Q*W)的迹
-    """
-    gama = 10^-6
-    return np.linalg.norm(np.dot(np.dot(H, X.T), W) - np.dot(H, Y))**2 + gama*np.trace(np.dot(np.dot(W.T, Q),W))
-
-
-def fun8_value(X, Y, W, b):
-    """
-    X : d x n
-    
-    ||X.T * W + 1*b.T - Y||的L2范数 ^ 2 + gama * ( ||W||的F范数 ^ 2 )
-    """
-    gama = 10^-6
-    n = X.shape[1]
-    return np.linalg.norm( np.dot(X.T,W) + np.dot(np.ones((n, 1)),b.T) - Y , ord=2)**2 + gama*(np.linalg.norm( W )**2)
-
-
-
-"""
-%% Problem
-%
-%  min  1/2 || x - v||^2
-%  s.t. x>=0, 1'x=k
-"""
-def EProjSimplex_new(v, k=1):    
-    v = np.matrix(v)
-    ft = 1;
-    n = np.max(v.shape)
-    
-#    if len(v.A[0]) == 0:
-#        return v, ft
- 
-    if np.min(v.shape) == 0:
-        return v, ft
-    
-#    print('n : ', n)
-#    print(v.shape)
-#    print('v :  ', v)
-    
-    v0 = v - np.mean(v) + k/n
-    
-#    print('v0 :  ', v0)
-    
-    vmin = np.min(v0)
-    
-    if vmin < 0:
-        f = 1
-        lambda_m = 0
-        while np.abs(f) > 10**-10:
-            v1 = v0 - lambda_m
-            posidx = v1 > 0
-            npos = np.sum(posidx)
-            g = -npos
-            f = np.sum(v1[posidx]) - k
-            lambda_m = lambda_m - f/g
-            ft = ft + 1
-            if ft > 100:
-                v1[v1<0] = 0.0
-                break
-        x = v1.copy()
-        x[x<0] = 0.0
-    else:
-        x = v0
-    return x,ft
-
-
-def get_W(X, Y):
-    
-    """
-    d特征，c类别，n样本数
-    
-    X : (d x n)
-    Y : (n x c)
-    
-    算法中使用的X的维度是(d x n)
-    """
-    
-    d, n = X.shape
-    c = Y.shape[1]
-    
-    Q = np.eye(d)
-    
-    """
-    H = I - 1/n * 1 * 1.T
-    """
-    H = np.eye(n,n) - 1/n*np.ones((n,n))
-    
-    
-    gama = 10^-6
-    """
-    W = (X*H*X.T + gama * Q)^-1 * X*H*Y
-    """
-    W = np.dot( np.dot( np.dot( \
-                np.linalg.inv( np.dot( np.dot(X,H), X.T)+2*Q ) \
-                               , X), H), Y)
-    """
-    q(ij) = ||W||2,1 / ||w^j||2
-    
-    axis = 1 =》 对W的每一行求L2范数
-    np.linalg.norm(W, ord=2, axis = 1) =》 对每行求L2范数
-    """
-    Q = np.sum(np.linalg.norm(W, ord=2, axis = 1)) / np.linalg.norm(W, ord = 2, axis=1)
-    Q = np.diag(Q)
-    
-    
-    
-    
-    pre_f = cur_f = fun22_value(W, X, H, Q, Y)    
-    
-    NITER = 900
-    epsilon = 10^-6
-    for i in range(NITER):
-        pre_f = cur_f
-        W = np.dot( np.dot( np.dot( \
-                np.linalg.inv( np.dot( np.dot(X,H), X.T)+2*Q ) \
-                               , X), H), Y)
-        Q = np.diag(np.linalg.norm(W, axis=1))
-        cur_f = fun22_value(W, X, H, Q, Y)
-        if (cur_f - pre_f) / cur_f < epsilon:
-            break
-    return W
-
-
+from LSFS_FUN import *
+from LSFS_TEST import *
+from read_data import *
+from evaluate import *
 
 file_path = "..\\..\\data_selected\\gene\\brain\\"
 
@@ -146,128 +19,35 @@ selected_cluster_name_file_name = "selected_cluster_names"
 
 unselected_data_file_name = "unselected_data"
 # unselected_feature_file_name = "unselected_features"
-# unselected_cluster_name_file_name = "unselected_cluster_names"
-
+unselected_cluster_name_file_name = "unselected_cluster_names"
 example_rate = 50
 feature_rate = 1
-unselected_example_rate = 100 - example_rate
-unselected_feature_rate = 100 - feature_rate
-selected_data_file_name = file_path + selected_data_file_name + "_" +  str(example_rate) + "_" + str(feature_rate) + "" + ".txt"
-# selected_feature_file_name = file_path + selected_feature_file_name + "_" + str(feature_rate) + "" + ".txt"
-selected_cluster_name_file_name = file_path + selected_cluster_name_file_name + "_"  + str(example_rate) + "" + ".txt"
 
-unselected_data_file_name = file_path + unselected_data_file_name + "_" +  str(unselected_example_rate) + "_" + str(feature_rate) + "" + ".txt"
+output_file_name = file_path + "lsfs_result" + "_" +  str(example_rate) + "_" + str(feature_rate) + "" + ".txt"
 
-output_file_name = file_path + "result" + "_" +  str(example_rate) + "_" + str(feature_rate) + "" + ".txt"
+XL_train, YL_train, XU_train, YU_train  = get_data(file_path, selected_data_file_name, selected_cluster_name_file_name, \
+                                    unselected_data_file_name, unselected_cluster_name_file_name, example_rate, feature_rate)
 
-print(selected_data_file_name)
-# print(selected_feature_file_name)
-print(selected_cluster_name_file_name)
-print(unselected_data_file_name)
-print(output_file_name)
+# feature_order, time_dual, a = run_accuracy(lsfs, XL_train,YL_train,XU_train,YU_train, 10, output_file_name)
 
+XL, YL, XU, YU = XL_train.copy(), YL_train.copy(), XU_train.copy(), YU_train.copy()
+YL = read_data.label_n1_to_nc(YL)
+YU = read_data.label_n1_to_nc(YU)
+feature_order, time_dual = lsfs(XL, YL, XU, output_file_name="feature_order")
 
-# 读入数据，分别是带标签，不带标签和标签
-# 读入数据，分别是带标签，不带标签和标签
-XL = pd.read_csv(selected_data_file_name,sep=",",header=None, engine='python')
-YL = pd.read_csv(selected_cluster_name_file_name,sep=",",header=None, engine='python')
-XU = pd.read_csv(unselected_data_file_name,sep=",",header=None, engine='python')
+num_feature = len(feature_order)
+if num_feature > 300:
+    num_feature = 300
+
+acc_array = cal_many_acc(XL_train, YL_train, XU_train, YU_train,\
+                           feature_order, num_feature = num_feature)
 
 
-# 类型转换
-XL = XL.values
-YL = YL.values
-XU = XU.values
-
-# 要改成一维的，否则会算法会报错，因为取出的每个特征也是一维的
-YL = np.reshape(YL, (1,-1))[0]
-
-
-
-# 要改成一维的，否则会算法会报错，因为取出的每个特征也是一维的
-YL = np.reshape(YL, (1,-1))[0]
-
-c = np.max(YL) + 1
-ln, d = XL.shape
-un =  XL.shape[0]
-
-tmp_YL = np.zeros((ln, c))
-for i,j in enumerate(YL):
-    tmp_YL[i,j] = 1
-YL = tmp_YL
-
-
-
-start_time = time.clock()
-
-X = XL.T # X : (d X n)
-Y = YL
-n = X.shape[1]
-W = get_W(X, Y)
-b = 1/n*(np.dot(Y.T, np.ones((n,1))) - np.dot(np.dot(W.T, X), np.ones((n,1))))
-
-X = XU.T
-YU = np.zeros((un, c))
-for i in range(X.T.shape[1]):
-    ad = np.dot(X[:,i:i+1].T, W) + b.T
-    ad_new, ft = EProjSimplex_new(ad)
-    YU[i:i+1,:] = ad_new.A
-
-
-X = sp.concatenate((XL, XU), axis = 0)
-Y = sp.concatenate((YL, YU), axis = 0)
-
-
-X = X.T
-cur_f = fun8_value(X, Y, W, b)
-
-
-NITER = 10
-epsilon = 10^-6
-for i in range(NITER):
-    pre_f = cur_f
-
-    W = get_W(X, Y)
-    n = X.shape[1]
-    
-#     print(Y.T.shape, np.ones((n,1)).shape , W.T.shape, X.T.shape, np.ones((n,1)).shape)
-    b = 1/n*(np.dot(Y.T, np.ones((n,1))) - np.dot(np.dot(W.T, X), np.ones((n,1))))
-
-    X = XU.T
-    for i in range(X.T.shape[1]):
-        """
-        min ( ||(xi.T) * W + b.T - yi.T||的F范数 ^ 2 )
-        s.t. yi>=0, 1*yi=1
-        """
-        ad = np.dot(X[:,i:i+1].T, W) + b.T
-        ad_new, ft = EProjSimplex_new(ad)
-        YU[i:i+1,:] = ad_new.A
-
-
-    X = sp.concatenate((XL, XU), axis = 0)
-    Y = sp.concatenate((YL, YU), axis = 0)
-    
-    X = X.T
-    cur_f = fun8_value(X, Y, W, b)
-
-    if (cur_f - pre_f) / cur_f < epsilon:
-        break
-        
-        
-s = np.zeros(d)
-W_L2_sum = np.sum([np.linalg.norm(w, ord=2) for w in W])
-s = np.linalg.norm(W, ord=2, axis = 1) / W_L2_sum
-feature_order = list( np.argsort(s) )
-
-print(time.clock() - start_time)
-    
-    
-s = np.zeros(d)
-W_L2_sum = np.sum([np.linalg.norm(w, ord=2) for w in W])
-s = np.linalg.norm(W, ord=2, axis = 1) / W_L2_sum
-feature_order = list( np.argsort(s) )
-
-
-with open(output_file_name, "w+") as result_file:
-    print("\n".join([str(w) for w in feature_order]), file=result_file)
 print(feature_order)
+print("===================================================================")
+print("===================================================================")
+# print("accuracy : ", a)
+print("time : ", time_dual)
+
+
+plot_array_like(acc_array, xlabel_name="number feature", ylabel_name="accuracy")
--- PRPC/PRPC.py
+++ PRPC/PRPC.py
@@ -5,11 +5,27 @@ import pandas as pd
 import scipy as sp
 import numpy as np
 import time
-from my_math import *
 
-"""
-输出特征的索引编号，输出顺序是被选择的顺序
-"""
+
+def append_module_path():
+    import sys
+    paths = [ \
+        "../gen_data",
+        "../evaluate",
+        "../read_data"
+    ]
+    
+    for path in paths:
+        if path not in sys.path:
+            sys.path.append(path)
+        
+append_module_path()
+
+
+from my_math import *
+from PRPC_FUN import *
+from read_data import *
+from evaluate import *
 
 file_path = "..\\..\\data_selected\\gene\\brain\\"
 
@@ -19,104 +35,34 @@ selected_cluster_name_file_name = "selected_cluster_names"
 
 unselected_data_file_name = "unselected_data"
 # unselected_feature_file_name = "unselected_features"
-# unselected_cluster_name_file_name = "unselected_cluster_names"
-
+unselected_cluster_name_file_name = "unselected_cluster_names"
 example_rate = 50
 feature_rate = 1
-unselected_example_rate = 100 - example_rate
-unselected_feature_rate = 100 - feature_rate
-selected_data_file_name = file_path + selected_data_file_name + "_" +  str(example_rate) + "_" + str(feature_rate) + "" + ".txt"
-# selected_feature_file_name = file_path + selected_feature_file_name + "_" + str(feature_rate) + "" + ".txt"
-selected_cluster_name_file_name = file_path + selected_cluster_name_file_name + "_"  + str(example_rate) + "" + ".txt"
-
-unselected_data_file_name = file_path + unselected_data_file_name + "_" +  str(unselected_example_rate) + "_" + str(feature_rate) + "" + ".txt"
 
 output_file_name = file_path + "prpc_result" + "_" +  str(example_rate) + "_" + str(feature_rate) + "" + ".txt"
 
-print(selected_data_file_name)
-# print(selected_feature_file_name)
-print(selected_cluster_name_file_name)
-print(unselected_data_file_name)
-print(output_file_name)
 
 
-# 读入数据，分别是带标签，不带标签和标签
-# 读入数据，分别是带标签，不带标签和标签
-label_data = pd.read_csv(selected_data_file_name,sep=",",header=None, engine='python')
-label = pd.read_csv(selected_cluster_name_file_name,sep=",",header=None, engine='python')
-unlabel_data = pd.read_csv(unselected_data_file_name,sep=",",header=None, engine='python')
+
+XL_train, YL_train, XU_train, YU_train  = get_data(file_path, selected_data_file_name, selected_cluster_name_file_name,\
+                        unselected_data_file_name, unselected_cluster_name_file_name, example_rate, feature_rate)
 
 
-# 类型转换
-label_data = label_data.values
-label = label.values
-unlabel_data = unlabel_data.values
 
-# 要改成一维的，否则会算法会报错，因为取出的每个特征也是一维的
-YL = np.reshape(label, (1,-1))[0]
+order_Fs, time_dual = prpc(XL_train, YL_train, XU_train, output_file_name="feature_order")
 
-F = set(range(label_data.shape[1]))
-Fs = set()
-order_Fs = []
-Fa = F - Fs
 
-# 选取的特征个数
-S = len(F)
-
-len_label_data = label_data.shape[0]
-data = sp.concatenate((label_data, unlabel_data), axis = 0)
-
-s = time.clock()
-
-for k in range(1,S+1):
-    max_pearson = float("-inf")
-    Fk = -1
+num_feature = len(order_Fs)
+if num_feature > 300:
+    num_feature = 300
     
-    
-    
-    for Fj in Fa:
-        
-        
-        
-        pearson1 = compute_pearson(data[:len_label_data,Fj], YL)
-        
+acc_array = cal_many_acc(XL_train, YL_train, XU_train, YU_train,\
+                           order_Fs, num_feature = num_feature)
 
-#         print(label_data[:,Fj])
-#         print(YL)
-#         print(pearson1)
-        
-#         print(Fj)
-
-        
-        pearson2_sum = 0
-        
-        for Fi in Fs:
-            pearson2 = compute_pearson(data[:,Fj], data[:,Fi])
-            pearson2_sum += pearson2
-        
-        
-        if k == 1:
-            pearson2_sum = 0
-        elif k > 1:
-            pearson2_sum /= k - 1
-            
-#         print(pearson1, pearson2_sum)
-        
-        pearson1 -= pearson2_sum
-        
-        if pearson1 > max_pearson:
-            max_pearson = pearson1
-            Fk = Fj
-    
-    
-    
-    Fs = Fs | {Fk}
-    Fa = Fa - {Fk}
-    
-    order_Fs.append(Fk)
-
-print("time: ", time.clock() - s)
-
-with open(output_file_name, "w+") as result_file:
-    print("\n".join([str(w) for w in order_Fs]), file=result_file)
 print(order_Fs)
+print("===================================================================")
+print("===================================================================")
+# print("accuracy : ", a)
+print("time : ", time_dual)
+
+plot_array_like(acc_array, xlabel_name="number feature", ylabel_name="accuracy")
